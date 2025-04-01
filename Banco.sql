7057bdce42f6b2d2179f7332299913bb58f285e4
Diogo <diogo.barbosa@edu.pe.senac.br>
2025-04-01 17:08:00 -0300

    Create Banco.sql

---
+++ Banco.sql
@@ -0,0 +1,14 @@
+-- Criação do banco de dados
+CREATE DATABASE app_simples;
+
+
+-- Seleciona o banco de dados
+USE app_simples;
+
+
+-- Criação da tabela usuario
+CREATE TABLE usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario VARCHAR(50) NOT NULL,
+    senha VARCHAR(50) NOT NULL
+);
